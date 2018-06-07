82fae2eadd84ccb5eb9e847d2066b58a9db4aed8
Jonathan Cronin <cronin_jonathan@bah.com>
2018-06-07 14:16:11 -0400

    Adds drop down to change between chapters at the top of test templates

--- js/main.js
+++ js/main.js
@@ -74,25 +74,5 @@ $(document).ready(function(){
 	//implement fastclick
 	FastClick.attach(document.body);
 
-	$('figure').each(function(){
-		var figure=$(this).attr('data-id');
-
-		$(this).prepend('Figure '+figure+' &mdash; ');
-		$(this).wrapInner('<figcaption></figcaption>');
-		var imageUrl='media/'+figure+'.jpg';
-		var img=$('<img src="'+imageUrl+'">');
-		$(this).prepend(img);
-	});
-
-	$('.alert-note').each(function(){
-		$(this).prepend('<h5>NOTE</h5>');
-	});
-
-	$('.alert-warning').each(function(){
-		$(this).prepend('<h5>WARNING</h5>');
-	});
-
-	$('.alert-caution').each(function(){
-		$(this).prepend('<h5>CAUTION</h5>');
-	});
+	
 });
